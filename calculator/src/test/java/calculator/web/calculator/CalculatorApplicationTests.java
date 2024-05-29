cbbdfb12c61494a0d748ce3fd96997417936c220
Belinda Zhu <97043010+SpringOions@users.noreply.github.com>
2024-05-29 15:01:09 -0700

    Add files via upload

---
+++ calculator/src/test/java/calculator/web/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.web.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
